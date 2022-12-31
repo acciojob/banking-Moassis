a13362914a5b231b93b26e1621b4184b2d9f3e20
Moassis <moassis.rommel@gmail.com>
2022-12-31 11:37:55 +0530

    1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,20 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            // CurrentAccount obj = new CurrentAccount("Erwin", 6000, "YyX");
+            // System.out.println(obj.getTradeLicenseId());
+            // obj.withdraw(500);
+            // obj.deposit(200);
+            // System.out.println(obj.getBalance());
 
+            BankAccount bo = new BankAccount("Rommel0", 500, 100);
+
+            bo.generateAccountNumber(123, 2);
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
