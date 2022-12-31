358565f1a3b60b6b0222f3b0d2f3319455cac8db
Moassis <moassis.rommel@gmail.com>
2022-12-31 11:56:24 +0530

    2

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -7,22 +7,34 @@ public class CurrentAccount extends BankAccount {
         // minimum balance is 5000 by default. If balance is less than 5000, throw
         // "Insufficient Balance" exception
         super(name, balance, 5000);
-        validateLicenseId(tradeLicenseId);
+        validateLicenseIdHelper(tradeLicenseId);
     }
 
     public String getTradeLicenseId() {
         return tradeLicenseId;
     }
 
-    String finalAnswer = null;
-
-    public void validateLicenseId(String tradeLicenseId) throws Exception {
+    public void validateLicenseId() throws Exception {
         // A trade license Id is said to be valid if no two consecutive characters are
         // same
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid
         // license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        try {
+            if (tradeLicenseId == null) {
+                throw new Exception("Valid License can not be generated");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    String finalAnswer = null;
+
+    public void validateLicenseIdHelper(String tradeLicenseId) throws Exception {
+
         try {
             for (int i = 0; i < tradeLicenseId.length(); i++) {
                 if (tradeLicenseId.charAt(i) >= 'A' && tradeLicenseId.charAt(i) <= 'Z') {
